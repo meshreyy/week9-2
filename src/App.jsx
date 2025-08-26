f3715b4922b001ba92af86e8853f37bb25b8878b
meshreyy <iamjhashreya@gmail.com>
2025-08-26 12:24:17 +0530

    adding useEffect Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,9 +9,6 @@ function App() {
   return (
     <>
       <div>
-        <i><b>
-          Clock
-        </b></i>
         <Counter></Counter>
       </div>
     </>
@@ -20,16 +17,27 @@ function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
+  {/*once on first render it gets initialized to 0 
+  this will not re-initialize count to 0
+*/}
 
-  setInterval (function () {
-    setCount(count+1);
-  },1000)
 
-return (
+{/*
+useEffect will render in loop but not the logic inside it
+Guarding our setInterval from re-renders
+*/}
+
+
+useEffect(function() {
+   setInterval(function () {
+    setCount(count => count + 1);
+  }, 1000)
+  console.log("mounted");
+}, []);
+
+  return (
     <div>
       <h1 id="text">{count}</h1>
-
-
     </div>
   )
 }
