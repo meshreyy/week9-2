91f9ae589ce273299019e85f6f85168a5e068005
meshreyy <iamjhashreya@gmail.com>
2025-09-02 14:07:31 +0530

    todo lists

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,29 @@ import viteLogo from '/vite.svg'
 
 function App() {
 
-  return (<div>
-    <Card>
-      <h1>Card Title</h1>
-      <p>This is some content inside card</p>
-    </Card>
-    <Card>
-      <h1>Another card</h1>
-      <p>This card has different content!</p>
-    </Card>
-  </div>)
+  const todos = [{
+    title: "Go to gym",
+    done: false
+  },
+  {
+    title: "eat lunch",
+    done: true
+  }];
 
-}
-
-
-function Card({ children }) {
-  return (<div style={{
-    border:'1px solid #ccc',
-    color:"black",
-    boxShadow:'2px 2px 5px rgba(0,0,0,0.1)',
-    padding:'10px',
-    margin:'10px'
-  }}>
-    {children}
-  </div>);
+  const TodoComponent = todos.map(todo => <Todo title={todo.title} done={todo.done} />)
+  return (
+    <div>
+      {TodoComponent}
+    </div>
+  );
 
 };
+
+function Todo({title, done }) {
+  return <div>
+    {title} - {done ? "Done!" : "Not Done!"}
+  </div>
+}
 
 
 
